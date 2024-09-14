a31524c3b06ca2a0a27de275d30da457a4ea543b
dev-s1808r <dev.s1808r@gmail.com>
2024-09-14 13:58:49 +0530

    created react project and created a basic sidebar layout. refactoring much needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	// Function to toggle the sidebar
+	const toggleSidebar = () => {
+		setIsSidebarOpen(!isSidebarOpen);
+	};
+
+	return (
+		<div className={`container ${isSidebarOpen ? 'sidebar-open' : ''}`}>
+			{/* Toggle button */}
+			<button className='toggle-button' onClick={toggleSidebar}>
+				☰
+			</button>
+			<aside className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+				{/* Sidebar content */}
+				<button className='close-button' onClick={toggleSidebar}>
+					&times;
+				</button>
+				<h2>Sidebar</h2>
+				<ul>
+					<li>Menu Item 1</li>
+					<li>Menu Item 2</li>
+					<li>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem
+						ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum
+						dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit
+						amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+						consectetur adipiscing elit..
+					</li>
+				</ul>
+			</aside>
+			<main className='content'>
+				{/* Main content */}
+				<h1>Main Content</h1>
+				<p style={{ fontSize: '48px' }}>
+					{/* Long content to demonstrate scrolling */}
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit..Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit
+					amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing
+					elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor
+					sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing
+					elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor
+					sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing
+					elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor
+					sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing
+					elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor
+					sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing
+					elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor
+					sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing
+					elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor
+					sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing
+					elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor
+					sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing
+					elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem
+					ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor
+					sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit..
+				</p>
+				{/* Repeat or add more content to ensure overflow */}
+			</main>
+		</div>
+	);
 }
 
 export default App;
